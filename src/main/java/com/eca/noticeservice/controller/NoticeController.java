3f2f78cf456ef9885922b73c0476fbc68f17041f
dilshag <dilshaperera1118@gmail.com>
2026-03-31 09:03:30 +0530

    Completed notice CRUD

--- src/main/java/com/eca/noticeservice/controller/NoticeController.java
+++ src/main/java/com/eca/noticeservice/controller/NoticeController.java
@@ -3,6 +3,78 @@ package com.eca.noticeservice.controller;
 import com.eca.noticeservice.entity.Notice;
 import com.eca.noticeservice.repo.NoticeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/notices")
+@RequiredArgsConstructor
+//@CrossOrigin(origins = "*")
+public class NoticeController {
+
+    private final NoticeRepository noticeRepository;
+
+    @PostMapping
+    public Notice save(@RequestBody Notice notice) {
+        return noticeRepository.save(notice);
+    }
+
+    @GetMapping
+    public List<Notice> getAll() {
+        return noticeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Notice> getById(@PathVariable String id) {
+        Optional<Notice> notice = noticeRepository.findById(id);
+
+        if (notice.isPresent()) {
+            return ResponseEntity.ok(notice.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Notice> update(@PathVariable String id, @RequestBody Notice updatedNotice) {
+        Optional<Notice> optionalNotice = noticeRepository.findById(id);
+
+        if (optionalNotice.isPresent()) {
+            Notice existingNotice = optionalNotice.get();
+            existingNotice.setTitle(updatedNotice.getTitle());
+            existingNotice.setContent(updatedNotice.getContent());
+
+            Notice savedNotice = noticeRepository.save(existingNotice);
+            return ResponseEntity.ok(savedNotice);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable String id) {
+        Optional<Notice> optionalNotice = noticeRepository.findById(id);
+
+        if (optionalNotice.isPresent()) {
+            noticeRepository.deleteById(id);
+            return ResponseEntity.ok("Notice deleted successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
+
+
+
+/*
+package com.eca.noticeservice.controller;
+
+import com.eca.noticeservice.entity.Notice;
+import com.eca.noticeservice.repo.NoticeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,4 +95,4 @@ public class NoticeController {
     public List<Notice> getAll() {
         return noticeRepository.findAll();
     }
-}
+}*/
